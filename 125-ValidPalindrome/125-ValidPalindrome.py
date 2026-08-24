470c5eec4c4c8fd9c5aed1942be2f12a11fd5178
gowsic m s <msgowsicneuro@gmail.com>
2026-08-24 12:20:56 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -0,0 +1,17 @@
+# Last updated: 8/24/2026, 12:20:56 PM
+class Solution(object):
+    def isPalindrome(self, s):
+        left =0
+        right = len(s)-1
+        while left < right :
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                    right -=1
+            if s[left].lower() != s[right].lower():
+
+                return False
+
+            left += 1
+            right -=1
+        return True
