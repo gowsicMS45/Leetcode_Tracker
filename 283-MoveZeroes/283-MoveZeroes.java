37e581fa4dc9dba444130b049ca869ae640d0c5e
gowsic m s <msgowsicneuro@gmail.com>
2026-08-24 12:20:19 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,16 @@
+// Last updated: 8/24/2026, 12:20:19 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int pos =0 ;
+        for (int i=0;i<nums.length;i++){
+            if (nums[i]!=0) {
+                nums[pos] = nums[i];
+                pos ++;
+            }
+        }
+        while (pos <nums.length) {
+            nums[pos]=0;
+            pos++;
+        }
+    }
+}
