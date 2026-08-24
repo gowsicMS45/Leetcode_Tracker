6cd9964b1b747e0e1f44d56662187b5dfd9c31c0
gowsic m s <msgowsicneuro@gmail.com>
2026-08-24 12:21:45 +0530

    Create 12-IntegerToRoman

---
+++ 12-IntegerToRoman/12-IntegerToRoman.py
@@ -0,0 +1,12 @@
+# Last updated: 8/24/2026, 12:21:44 PM
+class Solution(object):
+    def intToRoman(self, num):
+        Roman = ""
+        storeIntRoman = [[1000,"M"],[900,"CM"],[500,"D"],[400,"CD"],[100,"C"],[90,"XC"],[50,"L"],[40,"XL"],[10,"X"],[9,"IX"],[5,"V"],[4,"IV"],[1,"I"]]
+        for i in range (len(storeIntRoman)):
+            while num >=storeIntRoman[i][0]:
+                Roman += storeIntRoman[i][1]
+                num -= storeIntRoman[i][0]
+        return Roman
+
+        
