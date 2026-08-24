c937d59c0a26c2e00c8a977d0d16b4082cc4ae9d
gowsic m s <msgowsicneuro@gmail.com>
2026-08-24 12:21:23 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,9 @@
+# Last updated: 8/24/2026, 12:21:23 PM
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range (len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] +=1
+                return digits 
+            digits[i] =0
+        return [1] + digits
