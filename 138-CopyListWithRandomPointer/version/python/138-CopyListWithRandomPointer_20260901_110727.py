27e025fd7b9cc6a86ac9060eafec613be0e0a00d
gowsic m s <msgowsicneuro@gmail.com>
2026-09-01 11:07:31 +0530

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/version/python/138-CopyListWithRandomPointer_20260901_110727.py
@@ -0,0 +1,19 @@
+# Last updated: 9/1/2026, 11:07:27 AM
+class Solution(object):
+    def copyRandomList(self, head):
+        if not head:
+            return None
+
+        mp = {}
+        cur = head
+
+        while cur:
+            mp[cur] = Node(cur.val)
+            cur = cur.next
+        cur = head
+        while cur:
+            mp[cur].next = mp.get(cur.next)
+            mp[cur].random = mp.get(cur.random)
+            cur = cur.next
+
+        return mp[head]
