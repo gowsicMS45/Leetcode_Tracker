915cece5ff21c90d15a65af966a22e973716c9a0
gowsic m s <msgowsicneuro@gmail.com>
2026-09-07 12:10:37 +0530

    Create 2473-MaxSumOfAPairWithEqualSumOfDigits

---
+++ 2473-MaxSumOfAPairWithEqualSumOfDigits/version/python/2473-MaxSumOfAPairWithEqualSumOfDigits_20260907_121041.py
@@ -0,0 +1,17 @@
+# Last updated: 9/7/2026, 12:10:41 PM
+class Solution(object):
+    def maximumSum(self, nums):
+        mp=defaultdict(list)
+        for num in nums:
+            digit_sum=sum(int(d) for d in str(num))
+            heapq.heappush(mp[digit_sum],-num)
+        ans=-1
+        for values in mp.values():
+            if len(values) > 1:
+                max1 =-heapq.heappop(values)
+                max2=-heapq.heappop(values)
+                ans=max(ans,max1+max2)
+        return ans
+
+
+        
