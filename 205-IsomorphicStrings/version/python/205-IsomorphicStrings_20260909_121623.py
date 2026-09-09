6a5f8b15817e20cf1d4036becafc74f408c851a1
gowsic m s <msgowsicneuro@gmail.com>
2026-09-09 12:16:23 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/version/python/205-IsomorphicStrings_20260909_121623.py
@@ -0,0 +1,12 @@
+# Last updated: 9/9/2026, 12:16:23 PM
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
